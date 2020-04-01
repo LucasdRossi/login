5c27fe8ed6b7612c9304d90255539e3d8773b94d
lucasdRossi <rossi.lucas06@gmail.com>
2020-04-01 18:26:38 -0300

    separating projects

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node
-
-WORKDIR /usr/src/app
-
-COPY . .
-
-RUN npm install 
-
-CMD npm run start
--- microservice/src/index.js
+++ microservice/src/index.js
@@ -1,14 +1,16 @@
 require("dotenv/config");
 const express = require("express");
+const cors = require("cors");
 const cookieSession = require("cookie-session");
 
 const authRouter = require("./routes/auth");
 
 const app = express();
 
-const PORT = 4000;
-const HOST = "0.0.0.0";
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || "0.0.0.0";
 
+app.use(cors());
 app.use(express.json());
 app.use(
   cookieSession({
